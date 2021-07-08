49a99cfe8ad5da46f51afcb228ba9218b7f78a72
rakibmahbubkhan <rak.mbk@gmail.com>
2021-07-09 03:26:24 +0600

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (config('APP_ENV') === 'production') {
+        if(config('APP_ENV')!==('production')) {
             URL::forceScheme('https');
         }
     }
